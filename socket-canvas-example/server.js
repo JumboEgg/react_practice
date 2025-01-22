a26602bb5fea4d735989e1aa6d9c9e3f943024b3
허정은 <a1l1i1c5e@gmail.com>
2025-01-22 13:06:28 +0900

    socket.io 그림판 다양한 색으로 그리기 완성

---
+++ socket-canvas-example/server.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// static file(CSS 등)을 적용하도록 설정
+app.use(express.static(__dirname));
+
+// route hanler '/'를 홈으로 정의
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello World</h1>');
+    // 요청 수신 시 index.html 파일 전송
+    res.sendFile(join(__dirname, 'client.html'));
+});
+
+io.sockets.on('connection', (socket) => {
+    console.log('client connected');
+
+    // 그리기 정보 전달 시 수행
+    socket.on('message', (data) => {
+        // 그리기 정보를 연결된 모든 client에 반영한다.
+        switch (data['status']) {
+            case 'start':
+                io.sockets.emit('start', data);
+                break;
+            case 'draw': 
+                io.sockets.emit('draw', data);
+                break;
+            case 'end':
+                io.sockets.emit('end', data);
+            default:
+                break;
+        }
+    })
+});
+
+const port = 3000;
+
+server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+});
